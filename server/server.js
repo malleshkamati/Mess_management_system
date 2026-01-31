9a582f632acd68e04d6a2c9bde37077c0d6a3260
mallesh <malleshkamati0803@gmail.com>
2026-01-31 17:38:40 +0530

    removed cors

--- server/server.js
+++ server/server.js
@@ -8,11 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 app.use(express.json());
 
 // Routes
